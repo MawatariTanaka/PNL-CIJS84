ab74b60f113670d81a806f9e339d91f1d46e1124
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-05-12 19:05:58 +0700

    Add footer

--- 2023-05-10/Workbook/task-list/src/App.js
+++ 2023-05-10/Workbook/task-list/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import CreateTask from "./components/CreateTask";
 import TaskList from "./components/TaskList";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CreateTask /> <TaskList />
+        <CreateTask /> <TaskList /> <Footer />
       </div>
     );
   }
