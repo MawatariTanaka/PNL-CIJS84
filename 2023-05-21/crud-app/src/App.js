2c7e729ce93d5bf82b80032de847cab9690010fc
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-05-21 22:32:08 +0700

    Update textbook

--- 2023-05-21/crud-app/src/App.js
+++ 2023-05-21/crud-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Table } from "reactstrap";
+import { Container, Table, Input, Button } from "reactstrap";
 import Student from "./Student";
 import "./App.css";
 
@@ -10,13 +10,74 @@ class App extends Component {
       { id: 2, firstName: "Jacob", lastName: "Thornton", username: "@fat" },
       { id: 3, firstName: "Larry", lastName: "the Bird", username: "@twitter" },
     ],
+    newStudent: {
+      id: "",
+      firstName: "",
+      lastName: "",
+      username: "",
+    },
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState((prevState) => ({
+      newStudent: {
+        ...prevState.newStudent,
+        [name]: value,
+      },
+    }));
+  };
+
+  handleAddStudent = () => {
+    const { newStudent, students } = this.state;
+    const { id, firstName, lastName, username } = newStudent;
+
+    this.setState({
+      students: [...students, { id, firstName, lastName, username }],
+      newStudent: { id: "", firstName: "", lastName: "", username: "" },
+    });
   };
 
   render() {
-    const { students } = this.state;
+    const { students, newStudent } = this.state;
 
     return (
       <Container className="mt-5">
+        <div className="mt-2" style={{ display: "flex" }}>
+          <label>ID</label>
+          <Input
+            name="id"
+            value={newStudent.id}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className="mt-2" style={{ display: "flex" }}>
+          <label>First Name</label>
+          <Input
+            name="firstName"
+            value={newStudent.firstName}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className="mt-2" style={{ display: "flex" }}>
+          <label>Last Name</label>
+          <Input
+            name="lastName"
+            value={newStudent.lastName}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className="mt-2" style={{ display: "flex" }}>
+          <label>Username</label>
+          <Input
+            name="username"
+            value={newStudent.username}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <Button color="primary" onClick={this.handleAddStudent}>
+          Add Student
+        </Button>
         <Table hover>
           <thead>
             <tr>
