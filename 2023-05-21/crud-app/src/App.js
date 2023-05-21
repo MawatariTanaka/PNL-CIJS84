92128753ae6803deabbb490b318169749300b05e
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-05-21 21:28:41 +0700

    Import reactstrap

--- 2023-05-21/crud-app/src/App.js
+++ 2023-05-21/crud-app/src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <></>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button color="danger">Danger!</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
