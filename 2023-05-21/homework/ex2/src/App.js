8e41971509e1a7d923012e8146de18259af29c5d
Piceratop Ultimate <buinhatminh2005@gmail.com>
2023-05-24 07:23:40 +0700

    Update homework

--- 2023-05-21/homework/ex2/src/App.js
+++ 2023-05-21/homework/ex2/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class Led extends Component {
+  render() {
+    return (
+      <div className="led" style={{ backgroundColor: this.props.color }}></div>
+    );
+  }
+}
+
+export class App extends Component {
+  state = {
+    color: ["red", "yellow", "green"],
+    currentColorIndex: 0,
+  };
+  render() {
+    const leds = this.state.color.map((color, index) => {
+      const isCurrentColor = index === this.state.currentColorIndex;
+      const ledColor = isCurrentColor ? color : "white";
+      return <Led key={index} color={ledColor} />;
+    });
+
+    return (
+      <div className="App">
+        <button
+          className="next-btn"
+          onClick={() => {
+            this.setState({
+              currentColorIndex:
+                (this.state.currentColorIndex + 1) % this.state.color.length,
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Next
+        </button>
+        <div className="led-container">{leds}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
