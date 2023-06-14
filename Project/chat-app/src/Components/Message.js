abf521712ba383e575f9760c500aad55e4ca89f2
MawatariTanaka <buinhatminh2005@gmail.com>
2023-06-14 11:11:04 +0700

    Minimal update

--- Project/chat-app/src/Components/Message.js
+++ Project/chat-app/src/Components/Message.js
@@ -1,10 +1,10 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { ChatContext } from '../Context/chatContext';
 import CurrentMessage from './Message/CurrentMessage';
 import BlankMessage from './Message/BlankMessage';
 
 export default function Message() {
-    const { currentMessagingUser, dispatch } = useContext(ChatContext);
+    const { currentMessagingUser } = useContext(ChatContext);
     return (
         <div className="messaging-space">
             {currentMessagingUser ? <CurrentMessage /> : <BlankMessage />}
